7e124841e8171c51d7110e8fbdf49385e4f40add
nathaliab03 <nathaliabitencourt03@gmail.com>
2024-05-10 08:56:07 -0300

    Add project files.

---
+++ TaskSphere/Controllers/ToDoListsController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskSphere.Models;
+
+namespace TaskSphere.Controllers;
+
+public class ToDoListsController : Controller
+{
+    private static List<ToDoList> _toDoLists = new List<ToDoList>()
+    {
+        new ToDoList {ToDoListId = 1, TaskName = "Criar Controller", TaskDescription = "Criação do Controller", TaskStatus = "Em Andamento", TaskType = "Work", StartDate = new DateOnly(2024, 2, 25) },
+        new ToDoList {ToDoListId = 2, TaskName = "Academia", TaskDescription = "Ir a Academia", TaskStatus = "Em Andamento", TaskType = "Saúde", StartDate = new DateOnly(2024, 2, 25) },
+        new ToDoList {ToDoListId = 3, TaskName = "Projeto SpringBoot", TaskDescription = "Projeto Faculdade", TaskStatus = "Concluida", TaskType = "Faculdade", StartDate = new DateOnly(2024, 2, 25) },
+        new ToDoList {ToDoListId = 3, TaskName = "Projeto SpringBoot Longo", TaskDescription = "Projeto Faculdade", TaskStatus = "Concluida", TaskType = "Faculdade", StartDate = new DateOnly(2024, 2, 25) },
+    };
+
+    public IActionResult Index()
+    {
+        return View(_toDoLists);
+    }
+
+    [HttpGet]
+    public IActionResult Create() 
+    {
+        return View(); 
+    }
+
+    [HttpPost]
+    public IActionResult Create(ToDoList toDoList)
+    {
+        if(ModelState.IsValid)
+        {
+            toDoList.ToDoListId = _toDoLists.Count > 0 ? _toDoLists.Max(l => l.ToDoListId) + 1 : 1;
+            _toDoLists.Add(toDoList);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var existingToDoList = _toDoLists.FirstOrDefault(l => l.ToDoListId == id);
+        if (existingToDoList == null) 
+            return NotFound();
+
+       return View(existingToDoList);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(ToDoList toDoList)
+    {
+        if (ModelState.IsValid)
+        {
+            var existingToDoList = _toDoLists.FirstOrDefault(l => l.ToDoListId == toDoList.ToDoListId);
+            if(existingToDoList != null)
+            {
+                existingToDoList.TaskName = toDoList.TaskName;
+                existingToDoList.TaskDescription = toDoList.TaskDescription;
+                existingToDoList.TaskStatus = toDoList.TaskStatus;
+                existingToDoList.TaskType = toDoList.TaskType;
+                existingToDoList.StartDate = toDoList.StartDate;
+                existingToDoList.EndDate = toDoList.EndDate;
+            }
+            return RedirectToAction("Index");
+        }
+
+        return View(toDoList);
+    }
+
+    public IActionResult Detail(int id)
+    {
+        var toDoList = _toDoLists.FirstOrDefault(l => l.ToDoListId == id);
+        if(toDoList == null)
+            return NotFound();
+
+        return View(toDoList);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var toDoList = _toDoLists.FirstOrDefault(l => l.ToDoListId == id);
+        if(toDoList == null)
+            return NotFound();
+
+        _toDoLists.Remove(toDoList);
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult DoingTask() {
+
+        var doingTask = _toDoLists.Where(t => t.TaskStatus == "Em Andamento").ToList();
+
+        return View(doingTask);
+    }
+
+    public IActionResult TaskDone()
+    {
+        var doneTask = _toDoLists.Where(t => t.TaskStatus == "Concluida").ToList();
+
+        return View(doneTask);
+    }
+}
---
+++ TaskSphere/Models/ToDoList.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskSphere.Models
+{
+    public class ToDoList
+    {
+        [Key]
+        public int ToDoListId { get; set; }
+
+        [Display(Name = "Tarefa")]
+        [Required]
+        public string? TaskName { get; set; }
+
+        [Display(Name = "Descrição")]
+        public string? TaskDescription { get; set; }
+
+        [Display(Name = "Status")]
+        [Required]
+        public string? TaskStatus { get; set; }
+
+        [Display(Name = "Categoria")]
+        public string? TaskType { get; set; }
+
+        [Display(Name = "Data de Início")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateOnly? StartDate { get; set; }
+
+        [Display(Name = "Data de Conclusão")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateOnly? EndDate { get; set; }
+    }
+}
